de2dceaa17ee5f87047c3094b7505b296e00c8ed
ejwaple <122317503+ejwaple@users.noreply.github.com>
2023-04-05 11:53:44 -0400

    .

--- js/main.js
+++ js/main.js
@@ -105,7 +105,7 @@ const scatterdata = d3.csv(
       })
       .attr("r", 6)
       .attr("fill-opacity", 0.75)
-      .attr("fill", "#7f7fff");
+      .attr("fill", "#8181ff");
 
     // update plot (when dropdown value is changed)
     function updatePlot(selectedX, selectedY) {
@@ -276,8 +276,8 @@ const scatterdata = d3.csv(
         d3.select(this)
           .attr("title", d.amount)
           .style("cursor", "default")
-          .style("fill-opacity", 0.75)
-          .style("fill", "#dad9ff");
+          .style("fill-opacity", 0.5)
+          .style("fill", "#a6a6ff");
         d3.select("#tooltip").style("left", "-9999px").style("top", "-9999px");
       })
       .on("click", function (d) {
